77f21bedb59d2f094be66435a56c0a3f7468ada3
peter_shen <petershen0307@hotmail.com>
2016-04-24 01:21:31 +0800

    add c unitest with minunit.h

--- fuzzy_search/README.md
+++ fuzzy_search/README.md
@@ -2,3 +2,6 @@
 
 calculate edit distance with [levenshtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance) algorithm
 * [reference](https://charles620016.hackpad.com/ep/pad/static/Japi4qFyAzt)
+
+## C unit test framework:
+* [MinUnit](http://www.jera.com/techinfo/jtns/jtn002.html)
--- fuzzy_search/main.c
+++ fuzzy_search/main.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "levenshtein_distance.h"
+#include "minunit.h"
+
+static char * all_tests();
+static char *  test_calc_distance();
+
+int tests_run = 0;
 
 int main()
 {
-    printf("hihi\n");
-    printf("%d\n", calc_distance("abcd", "abc"));
+    printf("Start testing\n");
+    char *result = all_tests();
+    if (result != 0)
+    {
+        printf("%s\n", result);
+    }
+    else
+    {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
+
+static char * all_tests()
+{
+    mu_run_test(test_calc_distance);
+    return 0;
+}
+
+static char * test_calc_distance()
+{
+    mu_assert("error, calc_distance(\"abc\", \"abc\") != 0", calc_distance("abc", "abc") == 0);
+    mu_assert("error, calc_distance(\"abcd\", \"abc\") != 1", calc_distance("abcd", "abc") == 1);
     return 0;
 }
