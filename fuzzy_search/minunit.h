543faca1c8bdd1f03f5ba05ede5b6582d5a6a265
peter_shen <petershen0307@hotmail.com>
2016-04-24 19:41:11 +0800

    add assert counting

--- fuzzy_search/minunit.h
+++ fuzzy_search/minunit.h
@@ -1,5 +1,6 @@
 /* file: minunit.h */
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; \
+#define mu_assert(message, test) do { ++assert_run; if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); ++tests_run; \
                                if (message) return message; } while (0)
+extern int assert_run;
 extern int tests_run;
